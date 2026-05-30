5c49f0cd29b473b47ad7c3765498b84d4b9e3003
Davi Fonseca Viana <daviviana2602@gmail.com>
2026-05-30 13:31:29 -0300

    init: initial commit

---
+++ src/test/java/br/com/davi/spring_boot_first/SpringBootFirstApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.davi.spring_boot_first;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFirstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
