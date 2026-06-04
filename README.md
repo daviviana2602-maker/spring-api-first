c48df21d327ae597f6099b0c8d7852c3e1e3973e
Davi Fonseca Viana <daviviana2602@gmail.com>
2026-06-04 19:30:48 -0300

    Readme created

---
+++ README.md
@@ -0,0 +1,26 @@
+# 🍰 CakeShop - API
+🚧 Em desenvolvimento
+
+API REST para gerenciamento de produtos, pedidos e usuários (admin/clientes).
+
+---
+
+## 🚀 Tecnologias
+- Java 21
+- Spring Boot
+- Spring Data JPA
+- Spring Security
+- PostgreSQL
+
+---
+
+## 📌 Funcionalidades
+- Cadastro de usuários (com hash de senha)
+- Login
+- CRUD de produtos
+
+---
+
+## ▶️ Como rodar
+```bash
+mvn spring-boot:run
--- src/main/java/br/com/davi/spring_boot_first/Security/SecurityConfig.java
+++ src/main/java/br/com/davi/spring_boot_first/Security/SecurityConfig.java
@@ -16,12 +16,12 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-    
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
-                .csrf(csrf -> csrf.disable())   // desliga proteção de formulário
+                .csrf(csrf -> csrf.disable())   
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().permitAll()   // libera todas as rotas da API
                 );
